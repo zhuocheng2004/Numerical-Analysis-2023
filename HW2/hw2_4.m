ff505485b0425267d27a02412e2cbe2905571a11
Zhuo Cheng <chengz21@mails.tsinghua.edu.cn>
2023-03-10 20:02:00 +0800

    Update - 202303102001

--- HW2/hw2_4.m
+++ HW2/hw2_4.m
@@ -3,4 +3,8 @@ format long;
 f1 = @(x) 1 ./ (1 + 25 .* x.^2);
 f2 = @(x) exp(-x.^2);
 
+figure(1);
 hw2_4_func1(f1);
+
+figure(2);
+hw2_4_func1(f2);
--- HW2/hw2_4_func1.m
+++ HW2/hw2_4_func1.m
@@ -1,20 +1,28 @@
 
 function hw2_4_func1(func)
-    Ns = 10:30;
+    Ns = 10:34;
+    errs = zeros(1, numel(Ns));
     for k = 1:numel(Ns)
         N = Ns(k);
         nodes_equi = -1 + (2 / N) * (0:N);
         nodes_cheb = hw2_4_chebpts(N);
         poly1 = hw2_4_lagrange(nodes_equi, func(nodes_equi));
         poly2 = hw2_4_lagrange(nodes_cheb, func(nodes_cheb));
-        pts = linspace(-1, +1, 1000);
+        pts = linspace(-1.0, +1.0, 1000);
         ys_equi = zeros(1, numel(pts));
         ys_cheb = zeros(1, numel(pts));
         for i = 1:numel(pts)
             ys_equi(i) = polyval(poly1, pts(i));
             ys_cheb(i) = polyval(poly2, pts(i));
         end
-        plot(pts, ys_cheb);
-        hold on;
+        err = 0;
+        for i = 1:numel(pts)
+            t = abs(ys_cheb(i) - func(pts(i)));
+            if t > err
+                err = t;
+            end
+        end
+        errs(k) = err;
     end
+    plot(Ns, errs);
 end
